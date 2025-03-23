3a17f853182375526ae00955db9ebd1730e7ce9e
alishba25 <alishba25ansari@gmail.com>
2025-03-23 21:25:11 +0530

    Final Commit after Deployment using Render

--- server.py
+++ server.py
@@ -1154,5 +1154,5 @@ def calculate_cognitive_scores(username):
         }
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
 
